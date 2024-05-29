8d3d51ccb71d8524e2c0ea3615bfb998e6033f41
DeathNet123 <arslan.computer2002@gmail.com>
2024-05-29 18:52:52 +0500

    done

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dartsec.com/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  site: 'https://dartforsec.github.io/dartsec-web/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  base: 'dartsec-web',
   sitemap: true, // Generate sitemap (set to "false" to disable)
   integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
 });
