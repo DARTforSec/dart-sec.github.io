675d495bdc898da6b943d81f50edd4771bbd41be
DeathNet123 <arslan.computer2002@gmail.com>
2024-05-29 19:03:31 +0500

    removed the deploy yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dartforsec.github.io/dartsec-web/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-  base: 'dartsec-web',
+  site: 'https://dartforsec.com/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true, // Generate sitemap (set to "false" to disable)
   integrations: [sitemap(), mdx(), lit(), icon()], // Add renderers to the config
 });
